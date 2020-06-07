d539ac854513dbdfafc52f026d5316db0ee5e1ef
grace-fantasy-1218 <55233324+grace-fantasy-1218@users.noreply.github.com>
2020-06-07 20:27:28 +0800

    Add files via upload

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'Grace');
+define('DBPASS', 'Grace1218');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
