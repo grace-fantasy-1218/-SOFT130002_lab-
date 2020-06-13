b89452254d9841b5d3d0c0989debba08d9ee2cf3
grace-fantasy-1218 <55233324+grace-fantasy-1218@users.noreply.github.com>
2020-06-13 14:11:38 +0800

    Add files via upload

---
+++ lab11/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'root');
+define('DBPASS', 'Grace1218');
+define('DBCONNSTRING','mysql:dbname=login;charset=utf8mb4;'); ?>
